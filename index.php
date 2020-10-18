e382dc1bf4953a4d5699375b760b5d54b601a9ce
Media Streaming Websites <support@s3bubble.com>
2020-10-18 23:20:44 +0100

    tests

--- index.php
+++ index.php
@@ -1,9 +1,8 @@
 <?php header("Access-Control-Allow-Origin: *"); ?>
 <html>
 <head>
-  <script type="text/javascript"
-      src="//www.gstatic.com/cast/sdk/libs/caf_receiver/v3/cast_receiver_framework.js">
-  </script>
+
+  <script type="text/javascript" src="//www.gstatic.com/cast/sdk/libs/caf_receiver/v3/cast_receiver_framework.js"></script>
 
 </head>
 <body>
@@ -14,38 +13,21 @@
 
   		const context = cast.framework.CastReceiverContext.getInstance();
 		const playbackConfig = new cast.framework.PlaybackConfig();
+		// Customize the license url for playback
+		playbackConfig.licenseUrl = 'https://widevine-dash.ezdrm.com/widevine-php/widevine-foreignkey.php?pX=CA6D8D';
+		playbackConfig.protectionSystem = cast.framework.ContentProtection.WIDEVINE;
+		playbackConfig.licenseRequestHandler = requestInfo => {
+		  requestInfo.withCredentials = true;
+		};
+		context.start({playbackConfig: playbackConfig});
 
-		context.getPlayerManager().setMessageInterceptor(
-            cast.framework.messages.MessageType.LOAD, loadRequestData => {
-            console.log('loadRequestData', loadRequestData);
-        });
-
-
+		// Update playback config licenseUrl according to provided value in load request.
 		context.getPlayerManager().setMediaPlaybackInfoHandler((loadRequest, playbackConfig) => {
-
-			console.log('loadRequest', loadRequest);
-
 		  if (loadRequest.media.customData && loadRequest.media.customData.licenseUrl) {
 		    playbackConfig.licenseUrl = loadRequest.media.customData.licenseUrl;
 		  }
 		  return playbackConfig;
 		});
-        
-
-		// Customize the license url for playback
-		playbackConfig.protectionSystem = cast.framework.ContentProtection.WIDEVINE;
-		playbackConfig.licenseRequestHandler = requestInfo => {
-		  requestInfo.withCredentials = true;
-		  requestInfo.headers = [
-		  	{
-		  		"origin": ["*"],
-		  		"responseHeader": ["Content-Type"],
-		  		"method": ["GET", "HEAD"],
-		  		"maxAgeSeconds": 86400
-		  	}
-		  ];
-		};
-		context.start({playbackConfig: playbackConfig});
 
   	</script>
 </body>
